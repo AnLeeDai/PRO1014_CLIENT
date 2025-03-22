3824e65ac99ab88b152cc837461ea80473e884db
Le Dai An <ledaian22@gmail.com>
2025-03-22 15:51:36 +0700

    feat:forgot password

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,90 +1,9 @@
-import { useState } from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Link,
-  Input,
-  Button,
-  Form,
-  addToast,
-} from "@heroui/react";
-import { useNavigate } from "react-router-dom";
+import { Card, CardHeader, CardBody, CardFooter, Link } from "@heroui/react";
 
-import useLogin from "@/hooks/api/useLogin";
 import { siteConfig } from "@/config/site";
 import AuthLayout from "@/layouts/auth";
 
 export default function ForgotPasswordPage() {
-  const [isError, setIsError] = useState<{
-    email?: string;
-    password?: string;
-  }>({});
-
-  const navigate = useNavigate();
-
-  const { mutate, isPending } = useLogin({
-    onError: (error) => {
-      addToast({
-        title: "Đăng nhập thất bại",
-        description: error.message,
-        color: "danger",
-      });
-    },
-
-    onSuccess: (data) => {
-      // save data to local storage
-      localStorage.setItem("user", JSON.stringify(data.data));
-
-      // redirect
-      navigate(siteConfig.route.home);
-    },
-  });
-
-  const handleSubmitForm = (e: {
-    preventDefault: () => void;
-    currentTarget: HTMLFormElement | undefined;
-  }) => {
-    e.preventDefault();
-    let formData = Object.fromEntries(new FormData(e.currentTarget));
-
-    let data = {
-      email: formData.email as string,
-      password: formData.password as string,
-    };
-
-    if (!data.email) {
-      validateEmail(data.email);
-
-      return;
-    } else if (!data.password) {
-      validatePassword(data.password);
-
-      return;
-    }
-
-    // call api login
-    mutate(data);
-  };
-
-  const validateEmail = (value: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(value)) {
-      setIsError({ email: "Email không hợp lệ" });
-    }
-  };
-
-  const validatePassword = (value: string) => {
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{6,}$/;
-
-    if (!passwordRegex.test(value)) {
-      setIsError({ password: "Mật khẩu không hợp lệ" });
-    }
-  };
-
   return (
     <AuthLayout>
       <Card
@@ -102,30 +21,9 @@ export default function ForgotPasswordPage() {
         </CardHeader>
 
         <CardBody>
-          <Form
-            className="space-y-4"
-            validationBehavior="aria"
-            validationErrors={isError}
-            onSubmit={handleSubmitForm}
-          >
-            <Input
-              isRequired
-              label="Email"
-              name="email"
-              size="md"
-              type="email"
-            />
-
-            <Button
-              className="w-full"
-              color="primary"
-              isLoading={isPending}
-              size="lg"
-              type="submit"
-            >
-              Đổi mật khẩu
-            </Button>
-          </Form>
+          <h1 className="text-center text-lg font-semibold">
+            Vui lòng liên hệ admin để đổi mật khẩu
+          </h1>
         </CardBody>
 
         <CardFooter>
