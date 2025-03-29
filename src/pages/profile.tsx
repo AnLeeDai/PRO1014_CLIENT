c9a224e7f2920d6a8fdfabed134e19430a8d1515
An Le Dai <ledaian22@gmail.com>
2025-03-29 16:06:35 +0700

    feat: api profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,31 +5,24 @@ import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
-  Link,
+  Form,
+  Skeleton,
 } from "@heroui/react";
-import { ArrowLeft } from "lucide-react";
 
 import DefaultLayout from "@/layouts/default";
 import { siteConfig } from "@/config/site";
+import BackLink from "@/components/back-link";
+import useGetUserInfo from "@/hooks/api/useGetUserInfo";
 
 export default function ProfilePage() {
+  const { data: userInfo, isLoading: isLoadingUserInfo } = useGetUserInfo();
+
   return (
     <DefaultLayout>
       <div className="space-y-8">
         {/* Tiêu đề + nút quay lại */}
         <div className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
-          <Link
-            className="cursor-pointer"
-            color="foreground"
-            href={siteConfig.route.home}
-          >
-            <div className="flex items-center gap-3">
-              <ArrowLeft className="w-6 h-6 mr-1" />
-
-              <h1 className="text-2xl font-semibold">Thông tin cá nhân</h1>
-            </div>
-          </Link>
+          <BackLink href={siteConfig.route.home} label="Quay lại trang trước" />
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
@@ -41,51 +34,89 @@ export default function ProfilePage() {
 
             <CardBody>
               <div className="flex items-center justify-center gap-6">
-                <Avatar
-                  className="w-44 h-44 text-large"
-                  src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-                />
+                {isLoadingUserInfo ? (
+                  <Skeleton className="w-44 h-44 rounded-full" />
+                ) : (
+                  <Avatar
+                    className="w-44 h-44 text-large"
+                    src={userInfo?.data?.avatar_url}
+                  />
+                )}
               </div>
 
               <p className="text-sm text-muted-foreground mt-2 text-center">
                 Thêm ảnh đại diện. Kích thước đề xuất là 176×176px
               </p>
-            </CardBody>
 
-            <CardFooter>
-              <div className="w-full flex flex-col lg:flex-row gap-5 items-center">
+              <div className="mt-auto w-full flex flex-col lg:flex-row gap-5 items-center">
                 <Button fullWidth color="danger">
                   Xoá ảnh
                 </Button>
 
-                <Button fullWidth color="primary">
+                <Button fullWidth color="primary" type="submit">
                   Thay đổi ảnh
                 </Button>
               </div>
-            </CardFooter>
+            </CardBody>
           </Card>
 
-          {/* Cột phải: Thông tin cá nhân */}
           <Card>
             <CardHeader>
               <h2 className="text-lg font-medium">Thông tin cá nhân</h2>
             </CardHeader>
 
             <CardBody className="space-y-4">
-              <Input defaultValue="Jubéo" label="Tên" />
-              <Input defaultValue="Hernandez" label="Họ" />
-              <Input
-                defaultValue="jubeo.hernandez@scoppe.fr"
-                label="Địa chỉ email"
-              />
-              <Input defaultValue="07 82 26 62 62" label="Số điện thoại" />
-            </CardBody>
+              {isLoadingUserInfo ? (
+                <>
+                  <Skeleton className="h-14 w-full rounded-lg" />
+                  <Skeleton className="h-14 w-full rounded-lg" />
+                  <Skeleton className="h-14 w-full rounded-lg" />
+                  <Skeleton className="h-14 w-full rounded-lg" />
+                </>
+              ) : (
+                <Form>
+                  <Input
+                    disabled
+                    defaultValue={userInfo?.data?.username}
+                    label="Tên tài khoản"
+                  />
 
-            <CardFooter>
-              <Button className="w-full" color="primary">
-                Cập nhật
-              </Button>
-            </CardFooter>
+                  <Input
+                    defaultValue={userInfo?.data?.full_name}
+                    disabled={isLoadingUserInfo}
+                    label="Họ và tên"
+                  />
+
+                  <Input
+                    defaultValue={userInfo?.data?.email}
+                    disabled={isLoadingUserInfo}
+                    label="Địa chỉ email"
+                  />
+
+                  <Input
+                    defaultValue={userInfo?.data?.phone_number}
+                    disabled={isLoadingUserInfo}
+                    label="Số điện thoại"
+                  />
+
+                  <Input
+                    defaultValue={userInfo?.data?.address}
+                    disabled={isLoadingUserInfo}
+                    label="Địa chỉ"
+                  />
+
+                  <div className="mt-5 w-full flex flex-col lg:flex-row gap-5 items-center">
+                    <Button className="w-full" color="secondary">
+                      Đổi mật khẩu
+                    </Button>
+
+                    <Button className="w-full" color="primary" type="submit">
+                      Cập nhật
+                    </Button>
+                  </div>
+                </Form>
+              )}
+            </CardBody>
           </Card>
         </div>
       </div>
