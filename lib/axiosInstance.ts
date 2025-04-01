2d6a9c60623c6ec842271a2083555a6507c46c8f
An Le Dai <ledaian22@gmail.com>
2025-04-02 00:14:41 +0700

    fix api

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    // process.env.NEXT_PUBLIC_API_BASE_URL ||
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
     "http://localhost/PRO1014_SERVER/routes/",
   headers: {
     "Content-Type": "application/json",
