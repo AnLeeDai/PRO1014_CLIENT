12297503a892bd969170a4f01fbe5837c69848cc
An Le Dai <ledaian22@gmail.com>
2025-04-08 20:30:11 +0700

    fix api get user

--- hooks/useUserInfo.ts
+++ hooks/useUserInfo.ts
@@ -2,22 +2,25 @@ import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
 import { getUserInfo } from "../lib/api/user";
 
-import { ResponseSuccess } from "@/types/api";
-
 interface UserInfo {
-  username: string;
-  full_name: string;
-  email: string;
-  phone_number: string;
-  address: string;
-  avatar_url: string;
+  success: boolean;
+  message: string;
+  user: {
+    user_id: number;
+    username: string;
+    full_name: string;
+    email: string;
+    phone_number: string;
+    address: string;
+    avatar_url: string;
+    password_changed_at: null;
+    created_at: string;
+    role: string;
+  };
 }
 
-export const useUserInfo = (): UseQueryResult<
-  ResponseSuccess<UserInfo>,
-  Error
-> => {
-  return useQuery<ResponseSuccess<UserInfo>, Error>({
+export const useUserInfo = (): UseQueryResult<UserInfo, Error> => {
+  return useQuery<UserInfo, Error>({
     queryKey: ["user-info"],
     queryFn: getUserInfo,
   });
--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,18 +1,30 @@
+import Cookies from "js-cookie";
+
 import axiosInstance from "../axiosInstance";
 
-import { ResponseSuccess } from "@/types/api";
-
-export const getUserInfo = async (): Promise<
-  ResponseSuccess<{
+export const getUserInfo = async (): Promise<{
+  success: boolean;
+  message: string;
+  user: {
+    user_id: number;
     username: string;
     full_name: string;
     email: string;
     phone_number: string;
     address: string;
     avatar_url: string;
-  }>
-> => {
-  const res = await axiosInstance.get("?request=get-user-by-id");
+    password_changed_at: null;
+    created_at: string;
+    role: string;
+  };
+}> => {
+  const userId = Cookies.get("user_id");
+
+  if (!userId) {
+    throw new Error("Không tìm thấy user_id trong cookie");
+  }
+
+  const res = await axiosInstance.get(`?request=get-user-by-id&id=${userId}`);
 
   return res.data;
 };
